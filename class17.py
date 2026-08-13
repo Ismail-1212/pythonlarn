8d43570d025fb61beee4c91f09150ddf1cf6de89
muhammad ismail <m.ismail95876@gmail.comgit config --global user.nameismail-1212>
2026-08-13 19:42:41 +0500

    tuple methd

--- class17.py
+++ class17.py
@@ -1,15 +1,15 @@
 # tuple
 
-nam2 = ("a", 1 , "b" , 2, 14.5 , 13.5,)
-nam3 = ("ismail","hassan" , "ali")
-nma4 = (1,2,3,4,5)
-nam4 = (1.2 , 2.3 , 3.4 ,)
+# nam2 = ("a", 1 , "b" , 2, 14.5 , 13.5,)
+# nam3 = ("ismail","hassan" , "ali")
+# nma4 = (1,2,3,4,5)
+# nam4 = (1.2 , 2.3 , 3.4 ,)
 
-# list 
-a = [1,2,3,4]
-b = ["ismail" " hassan" " ali"]
-c = [3.4 , 2.3 , 1.2 ,]
-d = ["ismail" , 1 , 3.4 ,]
+# # list 
+# a = [1,2,3,4]
+# b = ["ismail" " hassan" " ali"]
+# c = [3.4 , 2.3 , 1.2 ,]
+# d = ["ismail" , 1 , 3.4 ,]
 
-print(type(a))
-print (type(nam2))
+# print(type(a))
+# print (type(nam2))
---
+++ class18.py
@@ -0,0 +1,29 @@
+# nam = int (input("sher your matric marks"))
+# for i in range (5):
+# # city names 
+#  ismail = ("karachi" , "lahore" , "islamabad" , "hydrabad" , "karachi" , ) 
+
+# Q 2
+# a = (10, 20, 30, 40, 50,) 
+# print (a[-1])
+
+# a =  ("a","b","c","d","e") 
+# print (a[1: 4])
+
+# a =  (1,2,3,4,5)
+# print(a[::-1])
+
+A = (5,3,5,5,2,5) 
+print(A.count(5))
+
+B = (100,200,300,400)
+
+print(B.index(300))
+
+C = ("pakistan" , "karachi",)
+
+D = ("John", 30, "Engineer")
+name , age ,prof = D
+print(name)
+print(age)
+print(prof)
