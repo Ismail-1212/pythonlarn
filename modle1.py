2b10021794c67d7f524d52fd252530b7e88ddddd
muhammad ismail <m.ismail95876@gmail.comgit config --global user.nameismail-1212>
2026-08-21 17:48:38 +0500

    modul 01

---
+++ modle1.py
@@ -0,0 +1,116 @@
+# # module1 assignment
+# #  Coding Exercises
+
+# # Q. 24
+
+# print("ismail") 
+# print("python MHK Academy") 
+
+# # # Q. 25
+
+# age = 1
+# print("age")
+
+# # # Q. 26
+
+# num1 = 10
+# num2 = 30
+# sum = num1 + num2
+# print (num2+num1)
+# print(sum)
+
+# # # Q.27
+
+# name =  (input ("enter your name "))
+# age = int (input("enter your age"))
+# print (f"hello{name} tomhari age {age}")
+
+# # # Q. 28
+
+# A = 10
+# B = 50
+# A, B = B, A
+# print (A,B)
+
+# # # Q. 29
+
+# user = int (input("enter your numbers"))
+# user2 = int (input("enter your numbers"))
+# sum2 = user + user2
+# print(sum2)
+
+# # Q.30
+
+# a = 10 
+# b = 3.5 
+# c = "ismail " 
+# d = True 
+# print(type(a), type(b), type(c), type(d))
+
+# # # 5 Debugging Exercises 
+
+# # # Debug 1 
+# # # print("Hello World"
+# print("Hello World")      
+
+# # # Debug 2
+
+# # # name = input("Enter name: ") 
+# # # print(name + 5)
+# name = input("Enter name") 
+# print(f"{name} 5")
+
+# # # Debug 3
+
+# # # 2name = "Ali" 
+# # # print(2name)
+# name = "Ali" 
+# print("name")
+
+# # #Debug 4
+# # # if = 10 
+# # # print(if)
+# A = 10 
+# print(A)
+
+# # # Debug 5 
+
+# # # age = int(input("Enter age: ")) 
+# # # print("Your age is" age) 
+# age = int(input("Enter age: ")) 
+# print(f"Your age is {age}") 
+
+
+# # ///// Mini Assignment ///////
+
+# name =(input("inter your name "))
+# age01 =  int (input("enter your age"))
+# fev = (input("enter your fv subjct"))
+# age02 = age01 + 5
+# print (age02)
+# print(type(name), type(age), type(fev), type(age))
+# print(f"your name is {name} your age is {age} your fev subjct is {fev}")
+
+
+# //// project //////
+
+name = input("show yours name")
+age = int (input("what is your age"))
+city = input("whts your city")
+profession = input("show me your  profession")
+age2 = 2026 - age 
+
+current_rank = 5
+target_rank = 1 
+
+current_rank ,target_rank = target_rank , current_rank
+
+
+print("======= PROFILE CARD =========")
+print(f"Name: {name}")
+print(f"age:{age}")
+print(f" city:{city}")
+print(f"proofssion:{profession}")
+print(f"birthywar {age2}")
+print(f"current_rank {current_rank}")
+print(f"target_rank{target_rank}")
