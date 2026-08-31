e4cb65ca46be2892751c0a286d569e6aa1239fde
muhammad ismail <m.ismail95876@gmail.comgit config --global user.nameismail-1212>
2026-08-31 19:40:44 +0500

    python

--- modle9.py
+++ modle9.py
@@ -1,31 +1,31 @@
-#  modle9
-# modle name = dictionary
-# coding exercises
-# Q 1
+# #  modle9
+# # modle name = dictionary
+# # coding exercises
+# # Q 1
 
-fruts = {"apple" : 100 , "mangoo" : 50 , "banana" : 30}
-name = {"name" : "ali" , "hassan " : "ismail" }
-print(type (fruts))
+# fruts = {"apple" : 100 , "mangoo" : 50 , "banana" : 30}
+# name = {"name" : "ali" , "hassan " : "ismail" }
+# print(type (fruts))
 
-cars = {"BMW11" : "M5" , "BMW1" : "M4" , "BMW2" : 1983 , "BMW3" : "i7"}
-countrys = {"karachi" : "NO 1" , "lahore" : "NO 2" , "islamabad" : "NO 3" ,}
-print(cars["BMW11"])
-# Q 2
+# cars = {"BMW11" : "M5" , "BMW1" : "M4" , "BMW2" : 1983 , "BMW3" : "i7"}
+# countrys = {"karachi" : "NO 1" , "lahore" : "NO 2" , "islamabad" : "NO 3" ,}
+# print(cars["BMW11"])
+# # Q 2
 
-fruts["fruts"] = "oringe"
-fruts["banana"] = 1022
-# Q3
+# fruts["fruts"] = "oringe"
+# fruts["banana"] = 1022
+# # Q3
 
-print(fruts.get("karachiiiiiiiiiiiiiiiiiii"))      
+# print(fruts.get("karachiiiiiiiiiiiiiiiiiii"))      
 
-# Q 4
+# # Q 4
 
-print(fruts.keys())
-print(fruts.values())
+# print(fruts.keys())
+# print(fruts.values())
 
-# Q 5
+# # Q 5
 
-print(fruts.items())
+# print(fruts.items())
 
-for Fruts in fruts:
-    print(fruts.items())
+# for Fruts in fruts:
+#     print(fruts.items())
