6dd5d1bd659196370b6415c8ffaa77d35f2a5ebd
muhammad ismail <m.ismail95876@gmail.comgit config --global user.nameismail-1212>
2026-09-01 19:42:48 +0500

    python

--- modle9.py
+++ modle9.py
@@ -3,29 +3,79 @@
 # # coding exercises
 # # Q 1
 
+
 # fruts = {"apple" : 100 , "mangoo" : 50 , "banana" : 30}
-# name = {"name" : "ali" , "hassan " : "ismail" }
-# print(type (fruts))
+# # name = {"name" : "ali" , "hassan " : "ismail" }
+# # fruts.update ({"apppppp" : 111111, "mangooooooooo" : 2222222 })
+# # print(fruts)
+# # print(type (fruts))
 
-# cars = {"BMW11" : "M5" , "BMW1" : "M4" , "BMW2" : 1983 , "BMW3" : "i7"}
-# countrys = {"karachi" : "NO 1" , "lahore" : "NO 2" , "islamabad" : "NO 3" ,}
-# print(cars["BMW11"])
-# # Q 2
+# # cars = {"BMW11" : "M5" , "BMW1" : "M4" , "BMW2" : 1983 , "BMW3" : "i7"}
+# # countrys = {"karachi" : "NO 1" , "lahore" : "NO 2" , "islamabad" : "NO 3" ,}
+# # print(cars["BMW11"])
+# # # Q 2
 
-# fruts["fruts"] = "oringe"
-# fruts["banana"] = 1022
-# # Q3
+# # fruts["fruts"] = "oringe"
+# # fruts["banana"] = 1022
+# # # Q3
 
-# print(fruts.get("karachiiiiiiiiiiiiiiiiiii"))      
+# # print(fruts.get("karachiiiiiiiiiiiiiiiiiii"))      
 
-# # Q 4
+# # # Q 4
 
-# print(fruts.keys())
-# print(fruts.values())
+# # print(fruts.keys())
+# # print(fruts.values())
 
-# # Q 5
+# # # Q 5
 
-# print(fruts.items())
+# # print(fruts.items())
 
-# for Fruts in fruts:
-#     print(fruts.items())
+# # for Fruts in fruts:
+# #     print(fruts.items())
+
+
+# # for keys , values in fruts:
+# #     print(f"{keys}->{values}")
+
+# # Q 6
+# apple = fruts.pop("apple")
+# print(fruts)
+# print(apple)
+
+# # student = {"name" = "Ali", "age": 20} 
+# # print(student) 
+# student = {"name" : "Ali", "age": 20}
+# print(student) 
+
+# # Q 7
+
+# student = {
+#     "studentno1" : {"name" : "ismail" , "age" : 17  },
+#     "studentno2" : {"name" : "hizar" , "age" : 18  }
+    
+# }
+# print(student["studentno1"]["name"])
+
+# # Q 8
+
+# student5 = {"name" : "hassan" , "age" : 11}
+# student5.clear()
+# print(student5)
+# # debug 1
+# # student = {"name": "Ali"} 
+# # print(student["age"]) 
+
+# # debug 2
+# student = {"name": "Ali"}
+# print(student.get("age")) 
+
+# # debug 3
+
+# # student = {"name": "Ali", "age": 20} keys = student.keys()
+# # print(keys[0]) 
+
+
+# student = {"name": "Ali", "age": 20} 
+# keys = student.keys() 
+# print(keys) 
+
