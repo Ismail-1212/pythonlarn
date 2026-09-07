e08f7dfe7605071ee168592033e03e68866d7bf3
muhammad ismail <m.ismail95876@gmail.comgit config --global user.nameismail-1212>
2026-09-07 19:26:39 +0500

    python

---
+++ class20.py
@@ -0,0 +1,22 @@
+ function
+1
+
+def wallcome ():
+    print("wallcome to python")
+wallcome()  
+2
+
+def addition(a = 5, b = "ismail"):
+    print(a + b)
+addition()    
+addition(1 , 1 )
+# 3
+     
+def student_info(name , age):
+    print(name , age )
+student_info("ismai" , 10)
+# 4
+
+def square(sqnumbr):
+    print(sqnumbr  ** 2)  
+square(5)       
---
+++ class21.py
@@ -0,0 +1,15 @@
+def moltipaction  (a , b):
+    print (a*b)
+moltipaction (10 , 5)   
+
+def  square (num1):
+    print(num1 * num1 )
+square (5)
+
+def student_details (name ,age , course):
+    print(name , age ,course)
+student_details ("ismail" , 10 , "python")
+
+def  calculate_marks(subject1, subject2, subject3):
+    print(subject1 +  subject2 + subject3)
+calculate_marks(100 , 50 , 99)    
