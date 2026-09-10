f65325f0cb9c4ba405e2cf3a95fd6ab641c52ec8
muhammad ismail <m.ismail95876@gmail.comgit config --global user.nameismail-1212>
2026-09-10 19:34:51 +0500

    mini assingment

--- modl10.py
+++ modl10.py
@@ -1,6 +1,6 @@
-# # # coding exersce
+# # # # coding exersce
 
-# # # Q 1
+# # # # Q 1
 
 # # def average(num1, num2):
 # #     return (num1 + num2) / 2
@@ -41,20 +41,81 @@
 # #     sumofnumbers(numbers - 1)
 # # sumofnumbers(1)    
     
-# def sum_tak(n):
-#     if n <= 0:
-#         return 0
-#     return n + sum_tak(n - 1)
+# # def sum_tak(n):
+# #     if n <= 0:
+# #         return 0
+# #     return n + sum_tak(n - 1)
 
-# number = 5
-# print(f"1 se {number} tak ka sum: {sum_tak(number)}")
+# # number = 5
+# # print(f"1 se {number} tak ka sum: {sum_tak(number)}")
 
 
-# # Q 7
+# # # Q 7
 
-# counters = 1 
-# def updatecounter ():
-#     global counters
-#     counters += 1
-# updatecounter()
-# print(counters)
+# # counters = 1 
+# # def updatecounter ():
+# #     global counters
+# #     counters += 1
+# # updatecounter()
+# # print(counters)
+
+
+# # # dibaging
+
+# # # 1
+
+
+# # # def greet(): print("Hello")
+
+# # def greet():
+# #     print("Hello")
+# # greet()
+
+# # # 2
+# # # def add(a, b):     return a + b   print(add(5))      
+
+# # def add(a, b):
+# #     return a + b   
+# # print(add(5 , 10))  
+
+# # # 3
+# # def square(n)     return n * n   print(square(4)) 
+
+# # def square(n):
+# #     return n * n   
+# # print(square(4)) 
+
+# # 4
+# # def total(*args):     print(args + 1)   total(1, 2, 3)
+
+# # def total(*args):
+# #     print(args , 1)   
+# # total(1, 2, 3)
+
+# # 5
+# # def show():     print(x)     x = 10   show() 
+
+# # def show():
+# #     x = 10
+# #     print(x)     
+    
+# # show() 
+
+# # mini assiningmant
+# # Q1
+# def studnt_maraks(sumbject1 = 0 , subject2 = 0 , subject3 = 0):
+#     total = sumbject1 + subject2 + subject3
+#     return(sumbject1+subject2+subject3)/3,total
+
+# print(studnt_maraks(1 , 1 , 1))
+
+# # Q5 
+# def studnt_maraks(sumbject1 = 0 , subject2 = 0 , subject3 = 0):
+#     total = sumbject1 + subject2 + subject3
+#     print("total" , total)
+#     avrege = total /3
+#     print (avrege , avrege)
+#     grade =  lambda avrege: "A" if avrege >= 80 else "B" if avrege >= 60 else "c" if avrege >= 60 else "faleee"
+#     print("grade", grade (avrege) )
+    
+# studnt_maraks(70 , 76 , 89 ) 
